b6f7c9180a750be6b9c3b35f816898343c7f2599
hikamfajduani <96283916+hikamfajduani@users.noreply.github.com>
2022-09-07 17:20:21 +0700

    Update views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -3,4 +3,9 @@ from wishlist.models import BarangWishlist
 
 # Create your views here.
 def show_wishlist(request):
-    return render(request, "wishlist.html")
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Hikam
+}
+    return render(request, "wishlist.html", context)
